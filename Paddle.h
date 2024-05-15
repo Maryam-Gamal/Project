a7fe591b02eed9d1e636c20c7cc53ad45322d148
SebaWahba <159119606+SebaWahba@users.noreply.github.com>
2024-05-15 19:37:46 +0300

    Update Paddle.h

--- Paddle.h
+++ Paddle.h
@@ -1,4 +1,16 @@
 #ifndef PADDLE_H
 #define PADDLE_H
 
+#include<QObject>
+#include<QGraphicsRectItem>
+
+class Paddle:public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+public:
+    Paddle(QGraphicsPixmapItem *parent = nullptr);
+    void keyPressEvent(QKeyEvent*event);
+    double paddleCenterX();
+};
+
 #endif // PADDLE_H
