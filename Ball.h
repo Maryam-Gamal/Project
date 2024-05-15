dc30d4745374caf522a60a134d2e54cdf876febc
mennaelsafty <159125325+mennaelsafty@users.noreply.github.com>
2024-05-15 19:23:43 +0300

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -1,4 +1,35 @@
 #ifndef BALL_H
 #define BALL_H
+#include <QGraphicsEllipseItem>
+#include <QGraphicsRectItem>
+#include <QObject>
+
+
+class Ball : public QObject, public QGraphicsEllipseItem
+{
+    Q_OBJECT
+public:
+    Ball(double baseVelocity, QObject *parent = nullptr);
+    ~Ball();
+    double getCenterX() const;
+
+private:
+    void reverseVelocity();
+    void handlePaddleCollision();
+    void handleBlockCollision();
+
+public slots:
+    void move();
+    void reset();
+
+signals:
+    void outOfReach();
+    void blockHit(int scoreOfBlock);
+
+private:
+    double xvelocity;
+    double yvelocity;
+    double baseVelocity;
+};
 
 #endif // BALL_H
