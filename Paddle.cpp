6607fbb1f1d2fe285a4d203ac1aca1c0e5b291ad
SebaWahba <159119606+SebaWahba@users.noreply.github.com>
2024-05-15 20:34:07 +0300

    Update Paddle-Seba2.cpp

--- Paddle.cpp
+++ Paddle.cpp
@@ -1,32 +1,35 @@
 #include "Paddle.h"
-#include<QGraphicsScene>
-#include<QKeyEvent>
+#include <QBrush>
+#include <QGraphicsScene>
+#include <QKeyEvent>
 
-// Constructor for Paddle class
-Paddle::Paddle(QGraphicsPixmapItem *parent)
+Paddle::Paddle(QObject *parent)
+    : QObject(parent)
 {
-    setPixmap(QPixmap(":/images/paddle.png")); // Set the pixmap for the paddle
+    setRect(0, 0, 130, 15);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::white);
+    setBrush(brush);
+    setPos(250,750);
 }
 
 //Function to return the center x-coordinate of the paddle
-double Paddle::paddleCenterX(){
-    return x() + pixmap().width() / 2;
+double Paddle::paddleCenterX()
+{
+    return x() + rect().width() / 2;
 }
 
-
 // Event handler for key press events
-void Paddle::keyPressEvent(QKeyEvent*event)
+void Paddle::keyPressEvent(QKeyEvent *event)
 {
     // Move the paddle left when the left arrow key is pressed
-     if(event->key()==Qt::Key_Left) // Ensure paddle does not move out of bounds
-    {
-        if(pos().x()>0)  // Ensure paddle does not move out of bounds
-            setPos(x()-30,y());  // Move the paddle left by 30 units
-    }
-    // Move the paddle right when the right arrow key is pressed
-    else if(event->key()==Qt::Key_Right)
-    {
-        if(pos().x()+100 < 600)  // Ensure paddle does not move out of bounds
-            setPos(x()+30,y());  // Move the paddle right by 30 units
+    if (event->key() == Qt::Key_Left) {
+        if (pos().x() > 0)   // Ensure paddle does not move out of bounds
+            setPos(x() - 30, y());
+    }// Move the paddle right when the right arrow key is pressed
+    else if (event->key() == Qt::Key_Right) {
+        if (pos().x() + 130 < 600)  // Ensure paddle does not move out of bounds
+            setPos(x() + 30, y());
     }
 }
