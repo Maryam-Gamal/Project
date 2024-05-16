53d30f19ee297214c0d84cc582ceafd6e85188fa
Maryam-Gamal <159136539+Maryam-Gamal@users.noreply.github.com>
2024-05-16 21:02:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QApplication>
+#include "Game.h"
+#include <QMessageBox>
+#include <QDebug>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+
+
+    Game *game = new Game(); // Create a new Game object
 
     return a.exec();
 }
