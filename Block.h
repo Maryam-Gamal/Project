9c919fce93e1412070a017a503018c290b6dc0d4
ghadasherif <135965326+ghadasherif@users.noreply.github.com>
2024-05-16 20:54:23 +0300

    ghadas commit Block.h

--- Block.h
+++ Block.h
@@ -1,4 +1,20 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QObject>
+
+class Block : public QObject, public QGraphicsRectItem {
+    Q_OBJECT
+public:
+    Block(int x, int y, QColor color, QObject* parent = nullptr); // Constructor
+    int getScore() { return 1;}
+private:
+    int x;
+    int y;
+    QColor color;
+
+};
+
 #endif // BLOCK_H
